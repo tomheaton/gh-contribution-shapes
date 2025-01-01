4ec907c00e8f21e3b00dfcdb928d06a5a7eabbed
Tom Heaton <thomas.m.heaton@gmail.com>
2025-01-01 17:13:00 +0000

    feat: winter theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-type Theme = 'official' | 'normal' | 'halloween';
+import { createSignal } from "solid-js";
 
-const daysInYear = 364;
+type Theme = 'official' | 'normal' | 'halloween' | 'winter';
+
+const daysInYear: number = 364;
 
 function generateContributionData() {
   return Array.from({ length: daysInYear }, () =>
-    // Random contribution count (0-4)
     Math.floor(Math.random() * 5)
   );
 };
 
 const colours = {
   official: {
-    0: '#0d1117',
+    // 0: '#0d1117',
+    0: '#ebedf0',
     1: '#9be9a8',
     2: '#40c463',
     3: '#30a14e',
     4: '#216e39',
-    default: '#0d1117',
+    // default: '#0d1117',
+    default: '#ebedf0',
   },
   normal: {
     0: '#ebedf0',
@@ -34,9 +37,17 @@ const colours = {
     4: '#d15600',
     default: '#ebedf0',
   },
+  winter: {
+    0: '#ebedf0',
+    1: '#c7e9ff',
+    2: '#64c4ed',
+    3: '#3490dc',
+    4: '#2779bd',
+    default: '#ebedf0',
+  }
 } as const;
 
-function getColour(count: number, theme: Theme) {
+function getColour(count: number, theme: Theme): string {
   return colours[theme][count] || colours[theme].default;
 };
 
@@ -61,12 +72,14 @@ function ContributionGrid(props: { theme: Theme }) {
   );
 };
 
-
 export default function App() {
   return (
     <main class="flex flex-col items-center justify-center gap-y-4 h-screen">
       <h1 class="text-2xl font-bold tracking-tighter">My Contributions</h1>
+      <ContributionGrid theme="official" />
       <ContributionGrid theme="normal" />
+      <ContributionGrid theme="halloween" />
+      <ContributionGrid theme="winter" />
     </main>
   );
 };
